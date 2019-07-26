b11d233aa032e32d8d3f9fad8b4f51b59a6ff3e2
Maria Chalkiadaki <mhalkiad@ics.forth.gr>
2019-07-26 15:00:07 +0300

    added XMLParser and copy

--- src/forth/ics/isl/XML/XMLParser.java
+++ src/forth/ics/isl/XML/XMLParser.java
@@ -52,10 +52,38 @@ public class XMLParser {
     }
     
     
+    public void copyXMLFile(String sourceFile, String destFile) throws ParserConfigurationException, SAXException, IOException {
+        
+        DocumentBuilderFactory dbFactoryDest = DocumentBuilderFactory.newInstance();
+	DocumentBuilder dBuilderDest = dbFactoryDest.newDocumentBuilder();
+        Document docDest = dBuilderDest.parse(new File(destFile));
+
+        //normalization
+        docDest.getDocumentElement().normalize();
+        
+        Node startingNode = getRootElement();
+        System.out.println("StartingNode:" + startingNode.toString());
+        
+        NodeList childNodes = startingNode.getChildNodes();
+        for(int i=0; i<childNodes.getLength(); i++)
+        {
+            System.out.println("Node: " + childNodes.item(i));
+            Node newNode = docDest.importNode(childNodes.item(i), true);
+            //docDest.adoptNode(newNode);
+            docDest.getElementsByTagName(destFile).item(0).appendChild(newNode);
+        }
+        
+    }
     
-    public String parseWholeXML(Node startingNode) {
+    
+    
+    public String parseWholeXML(Node startingNode) throws ParserConfigurationException, SAXException, IOException {
       
-        String xmlStr = new String();
+        //String xmlStr = new String();
+        
+        DocumentBuilderFactory dbFactoryDest = DocumentBuilderFactory.newInstance();
+	DocumentBuilder dBuilderDest = dbFactoryDest.newDocumentBuilder();
+        Document docDest = dBuilderDest.parse(new File("/home/mhalkiad/Desktop/oaiPmh.xml"));
         
         NodeList childNodes = startingNode.getChildNodes();
         for(int i=0; i<childNodes.getLength(); i++)
@@ -69,11 +97,14 @@ public class XMLParser {
             // trim() is used to ignore new lines and spaces elements.
                 if(!childNode.getTextContent().trim().isEmpty())
                 {
+                    System.out.println(childNode.toString());
                     System.out.println(childNode.getTextContent());
+                    
+                    docDest.getElementsByTagName("metadata").item(0).appendChild(childNode);
                 }
             }
         }
-        return xmlStr;   
+        return "OK";   
     }
     
     
--- src/forth/ics/isl/service/RegistryWS.java
+++ src/forth/ics/isl/service/RegistryWS.java
@@ -3,9 +3,16 @@ package forth.ics.isl.service;
 
 import forth.ics.isl.XML.XMLBuilder;
 import forth.ics.isl.XML.XMLParser;
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.TransformerConfigurationException;
@@ -23,7 +30,7 @@ import org.xml.sax.SAXException;
 public class RegistryWS {
 	
     @GET
-    public Response helloWorld() throws ParserConfigurationException, TransformerConfigurationException, TransformerException, SAXException, IOException {
+    public Response helloWorld(@QueryParam("xml-folder") String xmlFolder) throws ParserConfigurationException, TransformerConfigurationException, TransformerException, SAXException, IOException {
         
 //        XMLBuilder xmlBuilder = new XMLBuilder("/home/mhalkiad/Desktop/oaiPmh.xml");
 //        xmlBuilder.createOaipmhElement("xmlns", "xsi", "schemaLocation");
@@ -35,15 +42,37 @@ public class RegistryWS {
 //
 //        String output = xmlBuilder.exportXMLFile();
         
-        XMLParser xmlParser = new XMLParser("/home/mhalkiad/service.xml");
+        XMLParser xmlParser = new XMLParser("/home/mhalkiad/Desktop/xmlFiles/service.xml");
         //String output = xmlParser.getTypeElement();
-        Node rootElement = xmlParser.getRootElement();
+       // Node rootElement = xmlParser.getRootElement();
        //System.out.println(rootElement.getTextContent());
-        String output = xmlParser.parseWholeXML(rootElement);
+       // String output = xmlParser.parseWholeXML(rootElement);
        
-        System.out.println(output);
+        //System.out.println(output);
+        
+        xmlParser.copyXMLFile("/home/mhalkiad/Desktop/xmlFiles/service.xml", "/home/mhalkiad/Desktop/oaiPmh.xml");
+        
+        ArrayList<String> results = (ArrayList<String>) getFileNames(xmlFolder);
+        
+        for(int i=0; i<results.size(); i++)
+            System.out.println(results.get(i));
        
         return Response.status(200).entity("ok").build();
     }
-	
+    
+    
+    
+    private List<String> getFileNames(String xmlFolder) {
+    
+        List<String> results = new ArrayList<String>();
+        
+        File[] files = new File(xmlFolder).listFiles();
+        
+        for (File file : files) {
+            if (file.isFile()) {
+                results.add(file.getName());
+            }
+        }
+        return results;
+    }
 }
